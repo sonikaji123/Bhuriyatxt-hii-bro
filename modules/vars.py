92842ab82a8c8c8a6e10d6c66150242cfa300eb9
Sawai12 <109336735+Sawai12@users.noreply.github.com>
2024-02-06 18:06:20 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "26451206"
 api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "6803737514:AAH5kv0Kc8o7bKuU6L7ScIHQEQfG7KfHPc8"
+bot_token = "6504457957:AAGNTxZmpF_owmmaUdH3KMKxxN9vNRpa03k"
