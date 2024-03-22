8ca29f1398b3ad887d8260367fa43e1a52fcbccf
sonikaji123 <161281593+sonikaji123@users.noreply.github.com>
2024-03-22 21:26:10 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "29551974"
 api_hash = "9bca2aa867336f6d010dd740d55f30c0"
-bot_token = "6896324408:AAEU-Em9s9RTtiC0m7IO-SoqBXP3tUg-_cU"
+bot_token = "6589073649:AAEzdGMFwIe9FA_1u4t3othJj-N5ls0OPlE"
